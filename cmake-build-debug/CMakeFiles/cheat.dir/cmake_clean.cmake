097c26540abfd903d0a44a3fd3cb97389e5f5fde
cai@mac <hsinyaotsai@foxmail.com>
2020-09-13 21:15:22 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/cheat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cheat.dir/main.cpp.o"
+  "cheat"
+  "cheat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cheat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
